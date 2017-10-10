30db05eb11d485b58101d9b465af27ab5b0c7291
Firtina Ozbalikci <firtina14@gmail.com>
2017-10-10 21:18:26 +0100

    Prevent unwanted console logs in tests

--- tests/src/index.tsx
+++ tests/src/index.tsx
@@ -1,10 +1,21 @@
 import "source-map-support/register";
 
+import MockConsole from "console-expect";
 import dirtyChai = require("dirty-chai");
 
 chai.use(dirtyChai);
 
 describe("React Three Renderer", () => {
+  const mockConsole = new MockConsole();
+
+  beforeEach(() => {
+    mockConsole.wrapConsole();
+  });
+
+  afterEach("ensure no unwanted console messages have been logged", () => {
+    mockConsole.revert();
+  });
+
   require("./core");
   require("./examples");
 });
