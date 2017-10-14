dee3874e49267a8471e621e8e0b9734102b8cb69
Firtina Ozbalikci <firtina14@gmail.com>
2017-10-14 20:35:31 +0100

    Cleanup

--- src/app/React3.tsx
+++ src/app/React3.tsx
@@ -25,19 +25,10 @@ class ContextReceiver extends React.Component<IContextWrapperProps, any> {
   }
 
   public componentWillReceiveProps(nextProps: Readonly<IContextWrapperProps>, nextContext: any): void {
-    // super.componentWillReceiveProps(nextProps, nextContext);
     this.props.onContextUpdate(nextContext);
   }
 
-  /* tslint:disable */
-  // private _reactInternalFiber: IFiber;
-  /* tslint:enable */
-
   public render() {
-    // if (this._reactInternalFiber.return && this._reactInternalFiber.return.return) {
-    //   this._reactInternalFiber.return = this._reactInternalFiber.return.return;
-    // }
-
     return <react-three-renderer-proxy testProps={this.props.testProps} />;
   }
 }
@@ -73,59 +64,32 @@ class React3 extends PureComponent<IReact3Properties, any> {
 
     this.div = null;
 
-    // the canvas gets created here so that it can be rendered into even before the component gets mounted.
-    // this.canvas = document.createElement("canvas");
-
     this.renderCount = 0;
 
+    const createFakeElement = (name: any) => {
+      return ({
+        name,
+        setAttribute: () => {
+          if (this.div) {
+            if (this.props.contextPassThrough) {
+              ReactThreeRendererContext.childContextTypes = this.props.contextPassThrough;
+
+              ReactThreeRenderer.render(<ReactThreeRendererContext
+                context={this.passThroughContext}>
+                {this.props.children}
+              </ReactThreeRendererContext>, this.div);
+            } else {
+              ReactThreeRenderer.render(this.props.children as any, this.div);
+            }
+          }
+        },
+      });
+    };
+
     this.fakeDOMContainerInfo = {
-      appendChild: () => {
-        // the child is fake, but...
-        // for unknown reasons, this seems to be the best spot to render into r3r here.
-
-        // I'll need to study to find out why exactly.
-        // Until then, assume fiber sorcery.
-
-        // Also, priority hack to ensure lifecycle guarantees.. ( not sure if necessary?, to be confirmed )
-        // Basically doing this worked before and I just kept it.
-        // TODO try to remove priority elevation and see what happens.
-        // R3R.rendererInternal.performWithPriority(1, () => {
-        // console.log('appended childed');
-
-        if (this.div) {
-          // console.log('rendered on append child');
-          // R3R.render(this.props.children, this.canvas);
-        }
-
-        // });
-      },
       nodeType: document.ELEMENT_NODE,
       ownerDocument: {
-        createElement: (name: any) => {
-          return ({
-            name,
-            setAttribute: (/* ...args: any[] */) => {
-              // console.log('set attribute: ', ...args);
-
-              if (this.div) {
-                if (this.props.contextPassThrough) {
-                  ReactThreeRendererContext.childContextTypes = this.props.contextPassThrough;
-
-                  ReactThreeRenderer.render(<ReactThreeRendererContext
-                    context={this.passThroughContext}>
-                    {this.props.children}
-                  </ReactThreeRendererContext>, this.div);
-                } else {
-                  ReactThreeRenderer.render(this.props.children as any, this.div);
-                }
-              }
-            },
-          });
-        }, // fake element gets created here
-      },
-      setAttribute: () => {
-
-        // R3R.render(this.props.children, this.canvas);
+        createElement: createFakeElement, // fake element gets created here
       },
       tagName: "react-three-renderer-fake-container",
       removeChild(/* child: any */) {
@@ -139,7 +103,6 @@ class React3 extends PureComponent<IReact3Properties, any> {
   }
 
   public componentDidMount() {
-
     if (this.props.contextPassThrough) {
       ReactThreeRendererContext.childContextTypes = this.props.contextPassThrough;
 
@@ -163,8 +126,8 @@ class React3 extends PureComponent<IReact3Properties, any> {
   public render() {
     this.renderCount++;
 
-    // Create a fake element that will keep remounting every render.
-    // Whenever it gets remounted, we can use that as an opportunity to render into R3R context
+    // Create a fake element that will keep updating a property every render.
+    // Whenever the property gets updated, we can use that as an opportunity to render into R3R context
     // I am guessing that should be solved by the `implementation` property?
     // I.e. I'd love to get a callback for a moment to be able to call `R3R.render`.
 
