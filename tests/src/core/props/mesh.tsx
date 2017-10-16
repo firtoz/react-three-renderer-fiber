3fa78f675450dfedbae298d8bc493d575321d7ba
Firtina Ozbalikci <firtina14@gmail.com>
2017-10-16 20:06:55 +0100

    GH-13 Ensure elements and object references can coexist peacefully

---
+++ tests/src/core/props/mesh.tsx
@@ -0,0 +1,177 @@
+import {expect} from "chai";
+import * as React from "react";
+import * as Sinon from "sinon";
+import {BoxGeometry, Geometry, Material, Mesh, MeshBasicMaterial} from "three";
+import ReactThreeRenderer from "../../../../src/core/renderer/reactThreeRenderer";
+import {propsTarget} from "../props";
+
+describe("for mesh", () => {
+  it("should allow material and geometry as children", () => {
+    const meshRef = Sinon.spy();
+    const geometryRef = Sinon.spy();
+    const materialRef = Sinon.spy();
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+    >
+      <boxGeometry ref={geometryRef} width={5} height={10} depth={15} />
+      <meshBasicMaterial ref={materialRef} color={0xff0000} />
+    </mesh>, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+    const geometry: Geometry = geometryRef.lastCall.args[0];
+    const material: Material = materialRef.lastCall.args[0];
+
+    expect(mesh).to.be.instanceOf(Mesh);
+    expect(geometry).to.be.instanceOf(Geometry);
+    expect(material).to.be.instanceOf(Material);
+
+    expect(mesh.geometry).to.equal(geometry);
+    expect(mesh.material).to.equal(material);
+  });
+
+  it("should allow material and geometry elements as properties", () => {
+    const meshRef = Sinon.spy();
+    const geometryRef = Sinon.spy();
+    const materialRef = Sinon.spy();
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={<boxGeometry ref={geometryRef} width={5} height={10} depth={15} />}
+      material={<meshBasicMaterial ref={materialRef} color={0xff0000} />}
+    >
+    </mesh>, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+    const geometry: Geometry = geometryRef.lastCall.args[0];
+    const material: Material = materialRef.lastCall.args[0];
+
+    expect(mesh).to.be.instanceOf(Mesh);
+    expect(geometry).to.be.instanceOf(Geometry);
+    expect(material).to.be.instanceOf(Material);
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(geometry);
+    expect(mesh.material, "Material should have been assigned to the mesh").to.equal(material);
+  });
+
+  it("should allow material and geometry objects as properties", () => {
+    const meshRef = Sinon.spy();
+
+    const geometry = new BoxGeometry(5, 6, 7);
+    const material = new MeshBasicMaterial({color: 0xff000000});
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={geometry}
+      material={material}
+    >
+    </mesh>, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+
+    expect(mesh).to.be.instanceOf(Mesh);
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(geometry);
+    expect(mesh.material, "Material should have been assigned to the mesh").to.equal(material);
+  });
+
+  it("should allow updating from a geometry object to a geometry element", () => {
+    const meshRef = Sinon.spy();
+    const geometryRef = Sinon.spy();
+    const materialRef = Sinon.spy();
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={<boxGeometry ref={geometryRef} width={5} height={10} depth={15} />}
+      material={<meshBasicMaterial ref={materialRef} color={0xff0000} />}
+    />, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+    const geometry: Geometry = geometryRef.lastCall.args[0];
+    const material: Material = materialRef.lastCall.args[0];
+
+    expect(mesh).to.be.instanceOf(Mesh);
+    expect(geometry).to.be.instanceOf(Geometry);
+    expect(material).to.be.instanceOf(Material);
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(geometry);
+    expect(mesh.material, "Material should have been assigned to the mesh").to.equal(material);
+
+    const newGeometry = new BoxGeometry(10, 10, 10);
+    const newMaterial = new MeshBasicMaterial({color: 0x00FF00});
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={newGeometry}
+      material={newMaterial}
+    />, propsTarget);
+
+    expect(mesh.geometry, "Geometry should have been updated for the mesh").to.equal(newGeometry);
+    expect(mesh.material, "Material should have been updated for the mesh").to.equal(newMaterial);
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={<boxGeometry ref={geometryRef} width={50} height={50} depth={50} />}
+      material={<meshBasicMaterial ref={materialRef} color={0xff0000} />}
+    />, propsTarget);
+
+    const newerGeometry: Geometry = geometryRef.lastCall.args[0];
+    const newerMaterial: Geometry = materialRef.lastCall.args[0];
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(newerGeometry);
+    expect(mesh.material, "Material should have been assigned to the mesh").to.equal(newerMaterial);
+  });
+
+  it("should allow mixing of elements and objects", () => {
+    const meshRef = Sinon.spy();
+    const geometryRef = Sinon.spy();
+    const materialRef = Sinon.spy();
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={<boxGeometry ref={geometryRef} width={5} height={10} depth={15} />}
+      material={<meshBasicMaterial ref={materialRef} color={0xff0000} />}
+    />, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+    const geometry: Geometry = geometryRef.lastCall.args[0];
+    const material: Material = materialRef.lastCall.args[0];
+
+    expect(mesh).to.be.instanceOf(Mesh);
+    expect(geometry).to.be.instanceOf(Geometry);
+    expect(material).to.be.instanceOf(Material);
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(geometry);
+    expect(mesh.material, "Material should have been assigned to the mesh").to.equal(material);
+
+    const newGeometry = new BoxGeometry(10, 10, 10);
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={newGeometry}
+      material={<meshBasicMaterial ref={materialRef} color={0xff0000} />}
+    />, propsTarget);
+
+    expect(mesh.geometry, "Geometry should have been updated for the mesh").to.equal(newGeometry);
+    expect(mesh.material, "Material should have been updated for the mesh").to.equal(material);
+
+    const newMaterial = new MeshBasicMaterial({color: 0x00FF00});
+
+    ReactThreeRenderer.render(<mesh
+      ref={meshRef}
+
+      geometry={<boxGeometry ref={geometryRef} width={5} height={10} depth={15} />}
+      material={newMaterial}
+    />, propsTarget);
+
+    expect(mesh.geometry, "Geometry should have been assigned to the mesh").to.equal(geometryRef.lastCall.args[0]);
+    expect(mesh.material, "Material should have been updated for the mesh").to.equal(newMaterial);
+  });
+});
---
+++ tests/src/core/props/object3d.tsx
@@ -0,0 +1,262 @@
+import {expect} from "chai";
+import * as React from "react";
+import * as Sinon from "sinon";
+import {Euler, Mesh, Object3D, Quaternion, Vector3} from "three";
+import ReactThreeRenderer from "../../../../src/core/renderer/reactThreeRenderer";
+import {mockConsole} from "../../index";
+import {propsTarget} from "../props";
+
+describe("for types that extend object3d", () => {
+  it("should be set and updated", (done) => {
+    const meshRef = Sinon.spy();
+
+    ReactThreeRenderer.render(<mesh
+      name={"test-name"}
+
+      ref={meshRef}
+    />, propsTarget);
+
+    const mesh: Mesh = meshRef.lastCall.args[0];
+    expect(mesh.name).to.equal("test-name");
+
+    ReactThreeRenderer.render(<mesh
+      name={"updated-name"}
+    />, propsTarget);
+
+    expect(mesh.name).to.equal("updated-name");
+
+    done();
+  });
+});
+
+describe("for object3D", () => {
+  describe("lookAt", () => {
+    it("should be called when initially set", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(1, 2, 3)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+      expect(lookAtSpy.lastCall.args[0]).to.deep.equal(new Vector3(1, 2, 3));
+    });
+
+    it("should be called when updated", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(1, 2, 3)}
+      />, propsTarget);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(3, 4, 5)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+      expect(lookAtSpy.lastCall.args[0]).to.deep.equal(new Vector3(3, 4, 5));
+    });
+
+    it("should not be called when another property changes", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      const lookAtVector = new Vector3(1, 2, 3);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={lookAtVector}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        name="new name"
+
+        lookAt={lookAtVector}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+    });
+
+    it("should be called when position property changes", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      const lookAtVector = new Vector3(1, 2, 3);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={lookAtVector}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+
+      const positionVector = new Vector3(10, 20, 30);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        position={positionVector}
+
+        lookAt={lookAtVector}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+      expect(lookAtSpy.lastCall.args[0]).to.deep.equal(lookAtVector);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        name="new name"
+        position={positionVector}
+
+        lookAt={lookAtVector}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+    });
+
+    it("should not be called when the property is removed", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(1, 2, 3)}
+      />, propsTarget);
+
+      // even if the position property changes!
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        position={new Vector3(5, 10, 15)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+    });
+
+    it("should not be called when replaced by another rotation property", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(1, 2, 3)}
+      />, propsTarget);
+
+      // even if the position property changes!
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        rotation={new Euler(5, 10, 15)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(1);
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(20, 30, 40)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+      expect(lookAtSpy.lastCall.args[0]).to.deep.equal(new Vector3(20, 30, 40));
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        quaternion={new Quaternion(0, 1, 0, 1)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+    });
+
+    it("should give a warning when other rotation properties exist", () => {
+      const object3DRef = Sinon.spy();
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+      />, propsTarget);
+
+      const object3D: Object3D = object3DRef.lastCall.args[0];
+
+      const lookAtSpy = Sinon.spy(object3D, "lookAt");
+
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(1, 2, 3)}
+      />, propsTarget);
+
+      // even if the position property changes!
+
+      mockConsole.expectWarn("An object is being updated with both 'lookAt' and 'rotation' properties.\n" +
+        "Only 'lookAt' will be applied.");
+      ReactThreeRenderer.render(<object3D
+        ref={object3DRef}
+
+        lookAt={new Vector3(20, 30, 45)}
+        rotation={new Euler(5, 10, 15)}
+      />, propsTarget);
+
+      expect(lookAtSpy.callCount).to.equal(2);
+    });
+  });
+});
