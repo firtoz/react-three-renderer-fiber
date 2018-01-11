ec6ca57fd70cfa7337e76b681907647bca205ab4
Firtina Ozbalikci <firtina14@gmail.com>
2018-01-11 08:37:38 +0000

    Add missing viewport class

---
+++ src/core/renderer/utils/viewport.ts
@@ -0,0 +1,7 @@
+export default class Viewport {
+  constructor(public x: number,
+              public y: number,
+              public width: number,
+              public height: number) {
+  }
+}
