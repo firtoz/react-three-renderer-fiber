023f51861b578cbba434b0ee04120499f62c8edf
Nathan Bierema <nbierema@gmail.com>
2018-08-05 16:08:26 -0400

    Oops

--- src/core/renderer/hostDescriptors/common/ObjectWrapper.ts
+++ src/core/renderer/hostDescriptors/common/ObjectWrapper.ts
@@ -241,7 +241,6 @@ export class WrappedEntityDescriptor<TWrapper extends WrapperDetails<TProps, TIn
         false,
         null).withWantsRepaint(true);
     }
-    Object.keys();
 
     for (const propName of propNames) {
       this.propertyGroups[remountGroupName].properties.push(propName);
